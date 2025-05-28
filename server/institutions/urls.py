28fd5505f7afc466a80e63f3aa730112345cab29
Allan77-debug <allan.paredes@correounivalle.edu.co>
2025-05-28 09:50:32 -0500

    Correccion URLs

--- server/institutions/urls.py
+++ server/institutions/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('<int:institution_id>/reject/', InstitutionRejectView.as_view(), name='institution-reject'),
     path('approveUser/<int:institution_id>/<int:uid>/', InstitutionApproveUser.as_view(), name='institution-approve-user'),
     path('rejectUser/<int:institution_id>/<int:uid>/', InstitutionRejectUser.as_view(), name='institution-reject-user'),
-    path('listUser/<int:institution_id>/users/', InstitutionUsersView.as_view(), name='institution-users')
+    path('listUser/<int:institution_id>/users/', InstitutionUsersView.as_view(), name='institution-users'),
     path('login/', InstitutionLoginView.as_view(), name ='institution-login'),
-    path('<int:institution_id>/driver-applications/', DriverApplicationsListView.as_view(), name='driver-applications-list'),
+    path('<int:institution_id>/driver-applications/', DriverApplicationsListView.as_view(), name='driver-applications-list')
 ]
